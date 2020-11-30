11e7bd9aa2da951133aafa1635fc633d36f6298c
Daniel <55301388+danielfarah54@users.noreply.github.com>
2020-11-30 19:49:48 -0300

    Update ListagemPorCidadeBean.java

--- MVC/src/main/java/br/ufscar/dc/dsw/bean/ListagemPorCidadeBean.java
+++ MVC/src/main/java/br/ufscar/dc/dsw/bean/ListagemPorCidadeBean.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import br.ufscar.dc.dsw.dao.CidadeDAO;
 import br.ufscar.dc.dsw.dao.HotelDAO;
+
 import br.ufscar.dc.dsw.domain.Cidade;
 import br.ufscar.dc.dsw.domain.Hotel;
 
 public class ListagemPorCidadeBean {
 
-	public List<Cidade> getCidades() {
+	public List<Cidade> getHotelPorCidade() {
 		CidadeDAO dao = new CidadeDAO();
 		return dao.getAll();
 	}
@@ -17,11 +18,11 @@ public class ListagemPorCidadeBean {
 	public List<Hotel> getHotelPorCidade(String nome) {
 		HotelDAO dao = new HotelDAO();
 		List<Hotel> listaDeHoteisPorCidade;
-		if (nome.length() > 0)
+		if (nome.length() > 0) {
 			listaDeHoteisPorCidade = dao.getByNomeCidade(nome);
-		else
+		} else {
 			listaDeHoteisPorCidade = dao.getAll();
-		
+		}
 		return listaDeHoteisPorCidade;
 	}
 }
