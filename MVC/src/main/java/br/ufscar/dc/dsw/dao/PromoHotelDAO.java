d8fc07a87c555415d1d53170ae761535721ba1ba
Daniel <55301388+danielfarah54@users.noreply.github.com>
2020-11-30 19:59:02 -0300

    Update PromoHotelDAO.java

--- MVC/src/main/java/br/ufscar/dc/dsw/dao/PromoHotelDAO.java
+++ MVC/src/main/java/br/ufscar/dc/dsw/dao/PromoHotelDAO.java
@@ -8,26 +8,24 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import br.ufscar.dc.dsw.domain.Hotel;
 import br.ufscar.dc.dsw.domain.SiteReserva;
-import br.ufscar.dc.dsw.domain.PromoHotel;
 
-public class PromoHotelDAO extends GenericDAO {
+public class SiteReservaDAO extends GenericDAO {
+	
+	public void insert(SiteReserva sitereserva) {
 
-    public void insert(PromoHotel promohotel) {
-
-        String sql = "INSERT INTO PromoHotel (preco, iniciopromo, fimpromo, Hotel_id, SiteReserva_id) VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO SiteReserva (url, nome, telefone, email, senha) VALUES (?, ?, ?, ?, ?)";
 
         try {
             Connection conn = this.getConnection();
             PreparedStatement statement = conn.prepareStatement(sql);;
 
             statement = conn.prepareStatement(sql);
-            statement.setFloat(1, promohotel.getPreco());
-            statement.setString(2, promohotel.getIniciopromo());
-            statement.setString(3, promohotel.getFimpromo());
-            statement.setLong(4, promohotel.getHotel().getId());
-            statement.setLong(5, promohotel.getSitereserva().getId());
+            statement.setString(1, sitereserva.getUrl());
+            statement.setString(2, sitereserva.getNome());
+            statement.setString(3, sitereserva.getTelefone());
+            statement.setString(4, sitereserva.getEmail());
+            statement.setString(5, sitereserva.getSenha());
             statement.executeUpdate();
 
             statement.close();
@@ -37,12 +35,11 @@ public class PromoHotelDAO extends GenericDAO {
         }
     }
 
-    public List<PromoHotel> getAll() {
+    public List<SiteReserva> getAll() {
 
-        List<PromoHotel> listapromohotel = new ArrayList<>();
+        List<SiteReserva> listaSiteReserva = new ArrayList<>();
 
-        String sql = "SELECT * FROM PromoHotel p, Hotel h, SiteReserva s WHERE p.Hotel_id = h.id AND p.Sitereserva_id = s.id";
-        //SELECT * from PromoHotel p, Hotel h where p.Hotel_id = h.id order by p.id"
+        String sql = "SELECT * from SiteReserva";
 
         try {
             Connection conn = this.getConnection();
@@ -51,18 +48,13 @@ public class PromoHotelDAO extends GenericDAO {
             ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
                 Long id = resultSet.getLong("id");
-                float preco = resultSet.getFloat("preco");
-                String iniciopromo = resultSet.getString("iniciopromo");
-                String fimpromo = resultSet.getString("fimpromo");
-                Long hotel_id = resultSet.getLong(5);
-                String cnpj = resultSet.getString("cnpj");
-                Long sitereserva_id = resultSet.getLong(6);
                 String url = resultSet.getString("url");
-
-                Hotel hotel = new Hotel(hotel_id, cnpj);
-                SiteReserva sitereserva = new SiteReserva(sitereserva_id, url);
-                PromoHotel promohotel = new PromoHotel(id, preco, iniciopromo, fimpromo, hotel, sitereserva);
-                listapromohotel.add(promohotel);
+                String nome = resultSet.getString("nome");
+                String telefone = resultSet.getString("telefone");
+                String email = resultSet.getString("email");
+                String senha = resultSet.getString("senha");
+                SiteReserva siteReserva = new SiteReserva(id, url, nome, telefone, email, senha);
+                listaSiteReserva.add(siteReserva);
             }
 
             resultSet.close();
@@ -71,90 +63,17 @@ public class PromoHotelDAO extends GenericDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return listapromohotel;
+        return listaSiteReserva;
     }
     
-    public List<PromoHotel> getPromohotel(Long hotel_id) {
-
-        List<PromoHotel> listapromohotel = new ArrayList<>();
-
-        String sql = "SELECT * FROM PromoHotel p, Hotel h, SiteReserva s WHERE p.Hotel_id = ? AND p.Sitereserva_id = s.id";
+    public void delete(SiteReserva sitereserva) {
+        String sql = "DELETE FROM SiteReserva where id = ?";
 
         try {
             Connection conn = this.getConnection();
             PreparedStatement statement = conn.prepareStatement(sql);
 
-            statement.setLong(1, hotel_id);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                Long id = resultSet.getLong("id");
-                float preco = resultSet.getFloat("preco");
-                String iniciopromo = resultSet.getString("iniciopromo");
-                String fimpromo = resultSet.getString("fimpromo");;
-                String cnpj = resultSet.getString("cnpj");
-                Long sitereserva_id = resultSet.getLong(6);
-                String url = resultSet.getString("url");
-
-                Hotel hotel = new Hotel(hotel_id, cnpj);
-                SiteReserva sitereserva = new SiteReserva(sitereserva_id, url);
-                PromoHotel promohotel = new PromoHotel(id, preco, iniciopromo, fimpromo, hotel, sitereserva);
-                listapromohotel.add(promohotel);
-            }
-
-            resultSet.close();
-            statement.close();
-            conn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return listapromohotel;
-    }
-    
-    public List<PromoHotel> getPromoSite(Long sitereserva_id) {    	
-
-        List<PromoHotel> listapromohotel = new ArrayList<>();
-
-        String sql = "SELECT * FROM PromoHotel p, Hotel h, SiteReserva s WHERE p.Hotel_id = h.id AND p.Sitereserva_id = ?";
-        //SELECT * from PromoHotel p, Hotel h where p.Hotel_id = h.id order by p.id"
-
-        try {
-            Connection conn = this.getConnection();
-            PreparedStatement statement = conn.prepareStatement(sql);
-
-            statement.setLong(1, sitereserva_id);
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                Long id = resultSet.getLong("id");
-                float preco = resultSet.getFloat("preco");
-                String iniciopromo = resultSet.getString("iniciopromo");
-                String fimpromo = resultSet.getString("fimpromo");
-                Long hotel_id = resultSet.getLong(5);
-                String cnpj = resultSet.getString("cnpj");
-                String url = resultSet.getString("url");
-
-                Hotel hotel = new Hotel(hotel_id, cnpj);
-                SiteReserva sitereserva = new SiteReserva(sitereserva_id, url);
-                PromoHotel promohotel = new PromoHotel(id, preco, iniciopromo, fimpromo, hotel, sitereserva);
-                listapromohotel.add(promohotel);
-            }
-
-            resultSet.close();
-            statement.close();
-            conn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return listapromohotel;
-    }
-
-    public void delete(PromoHotel promohotel) {
-        String sql = "DELETE FROM PromoHotel where id = ?";
-
-        try {
-            Connection conn = this.getConnection();
-            PreparedStatement statement = conn.prepareStatement(sql);
-
-            statement.setLong(1, promohotel.getId());
+            statement.setLong(1, sitereserva.getId());
             statement.executeUpdate();
 
             statement.close();
@@ -164,52 +83,46 @@ public class PromoHotelDAO extends GenericDAO {
         }
     }
 
-    public void update(PromoHotel promohotel) {
-        String sql = "UPDATE PromoHotel SET preco = ?, iniciopromo = ?, fimpromo = ?";
-        sql += ", Hotel_id = ?, Sitereserva_id = ? WHERE id = ?";
+    public void update(SiteReserva sitereserva) {
+        String sql = "UPDATE SiteReserva SET url = ?, nome = ?, telefone = ?, email = ?, senha = ? WHERE id = ?";
 
         try {
             Connection conn = this.getConnection();
             PreparedStatement statement = conn.prepareStatement(sql);
 
-            statement.setFloat(1, promohotel.getPreco());
-            statement.setString(2, promohotel.getIniciopromo());
-            statement.setString(3, promohotel.getFimpromo());
+            statement.setString(1, sitereserva.getUrl());
+            statement.setString(2, sitereserva.getNome());
+            statement.setString(3, sitereserva.getTelefone());
+            statement.setString(4, sitereserva.getEmail());
+            statement.setString(5, sitereserva.getSenha());
+            statement.setLong(6, sitereserva.getId());
+            statement.executeUpdate();
+
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public SiteReserva get(Long id) {
+    	SiteReserva siteReserva = null;
+        
+        String sql = "SELECT * from SiteReserva where id = ?";
+
+        try {
+            Connection conn = this.getConnection();
+            PreparedStatement statement = conn.prepareStatement(sql);
             
-            statement.setFloat(5, promohotel.getHotel().getId());
-            statement.setFloat(6, promohotel.getSitereserva().getId());
-            statement.setLong(7, promohotel.getId());
-            statement.executeUpdate();
-
-            statement.close();
-            conn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public PromoHotel get(Long id) {
-        PromoHotel promohotel = null;
-
-        String sql = "SELECT * from PromoHotel p, Hotel h, SiteReserva s WHERE p.id = ? and p.Hotel_id = h.id and p.Sitereserva_id = s.id";
-
-        try {
-            Connection conn = this.getConnection();
-            PreparedStatement statement = conn.prepareStatement(sql);
-
             statement.setLong(1, id);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                float preco = resultSet.getFloat("preco");
-                String iniciopromo = resultSet.getString("iniciopromo");
-                String fimpromo = resultSet.getString("fimpromo");
-
-                Long hotelID = resultSet.getLong("Hotel_id");
-                Hotel hotel = new HotelDAO().get(hotelID);
-                
-                Long SitereservaID = resultSet.getLong("SiteReserva_id");
-                SiteReserva sitereserva = new SiteReservaDAO().get(SitereservaID);
-                promohotel = new PromoHotel(id, preco, iniciopromo, fimpromo, hotel, sitereserva);
+                String url = resultSet.getString("url");
+                String nome = resultSet.getString("nome");
+                String telefone = resultSet.getString("telefone");
+                String email = resultSet.getString("email");
+                String senha = resultSet.getString("senha");
+                siteReserva = new SiteReserva(id, url, nome, telefone, email, senha);
             }
 
             resultSet.close();
@@ -218,46 +131,26 @@ public class PromoHotelDAO extends GenericDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return promohotel;
-    }
-
-    public int countByHotel(Long id) {
-        int contador = 0;
-        
-        String sql = "SELECT count(*) from PromoHotel p where p.Hotel_id = ?";
-
-        try {
-            Connection conn = this.getConnection();
-            PreparedStatement statement = conn.prepareStatement(sql);
-
-            statement.setLong(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                contador = resultSet.getInt(1);
-            }
-
-            resultSet.close();
-            statement.close();
-            conn.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return contador;
+        return siteReserva;
     }
     
-    public int countBySiteReserva(Long id) {
-        int contador = 0;
-        
-        String sql = "SELECT count(*) from PromoHotel p where p.Sitereserva_id = ?";
+    public SiteReserva getbyLogin(String email) {
+    	SiteReserva siteReserva = null;
+
+        String sql = "SELECT * from SiteReserva WHERE email = ?";
 
         try {
             Connection conn = this.getConnection();
             PreparedStatement statement = conn.prepareStatement(sql);
-
-            statement.setLong(1, id);
+            statement.setString(1, email);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                contador = resultSet.getInt(1);
+            	Long id = resultSet.getLong("id");
+                String url = resultSet.getString("url");
+                String nome = resultSet.getString("nome");
+                String telefone = resultSet.getString("telefone");
+                String senha = resultSet.getString("senha");
+                siteReserva = new SiteReserva(id, url, nome, telefone, email, senha);
             }
 
             resultSet.close();
@@ -266,6 +159,6 @@ public class PromoHotelDAO extends GenericDAO {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return contador;
+        return siteReserva;
     }
 }
